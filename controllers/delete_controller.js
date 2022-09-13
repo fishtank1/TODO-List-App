c7299315275b68862fc1563aba2551a2d7cdff6f
Soumyajeet <senguptasaumyajeet@outlook.com>
2022-09-13 12:41:47 +0530

    bugs with delete task fixed

--- controllers/delete_controller.js
+++ controllers/delete_controller.js
@@ -1,12 +1,23 @@
 const Task = require("../models/task");
 
 module.exports.delete = function(req, res) {
-    Task.findByIdAndDelete(req.body.id, (err) => {
-        if (err){
-            console.log('Error in finding the selected id..', err)
-        }
-        else{
-            return res.redirect('back');
-        }
-    });
+    // console.log();
+    if(Array.isArray(req.body.id)) {
+        (req.body.id).map((data) => {
+            Task.findByIdAndDelete(data, (err) => {
+                if (err){
+                    console.log('Error in finding the selected id..', err)
+                }
+            });
+        });
+        return res.redirect('back');
+    } else {
+        Task.findByIdAndDelete(req.body.id, (err) => {
+            if (err){
+                console.log('Error in finding the selected id..', err)
+            } else {
+                return res.redirect('back');
+            }
+        });
+    }
 }
